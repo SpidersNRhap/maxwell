391cd799401ffc6e6de49ef5896819c258a1d0e3
SpidersNRhap <nahum.cortezzo@gmail.com>
2026-03-31 16:02:07 -0700

    added the last player spawned bubble to the item position tracker

--- dll/ui.cpp
+++ dll/ui.cpp
@@ -570,6 +570,12 @@ void UI::DrawItemStates() {
       // ImGui::InputFloat2("Slink##PlayerSlinkPosition", &((FVec2 *)(*(size_t *)get_address("slots")+0x9B5E8))->x);
       ImGui::InputFloat2("Wheel##PlayerWheelPosition",
                          &Max::get().player_wheel()->x);
+      int32_t* last_bubble_ptr =(int32_t*)(*(size_t*)get_address("slots")+0x9B128);
+      int32_t last_bubble_address = *last_bubble_ptr;
+      if (last_bubble_address != 0) {
+        FVec2* last_bubble = reinterpret_cast<FVec2*>(last_bubble_address);
+        ImGui::InputFloat2("LastBubble##LastSpawnedBubble", &(last_bubble)->x);
+      }
       ImGui::EndTabItem();
     }
     if (ImGui::BeginTabItem("Firecrackers")) {
